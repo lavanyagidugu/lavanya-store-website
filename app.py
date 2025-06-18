9bb6e56231641ff1f1a0d84fadafe42e2f2cd891
lavanyagidugu <118426634+lavanyagidugu@users.noreply.github.com>
2025-06-18 21:38:15 +0000

    Added dynamic data and app route

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+JOBS=[
+    {'product id' : '123456789',
+    'product_name' : 'Traditional wear Dresses'
+    },
+    {'product id' : '123456756',
+    'product_name' : 'Saree'
+    },
+    {'product id' : '123459890',
+    'product_name' : 'Western Wear'
+    },
+    {'product id' : '123876789',
+    'product_name' : 'Suites'
+    }
+    
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",jobs=JOBS,company_name="LavanyaBalu")
 
-
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
